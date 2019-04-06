527c64bb582473417554d98b173671ad82394567
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-04-06 10:56:08 +0300

    Split long line.

--- speaker/speaker_test.go
+++ speaker/speaker_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestEffect(t *testing.T) {
-	expected := `<speaker effect="behind_the_wall">Behind!<speaker effect="-"><speaker audio="alice-sounds-game-win-1.opus">`
+	expected := `<speaker effect="behind_the_wall">Behind!<speaker effect="-">` +
+		`<speaker audio="alice-sounds-game-win-1.opus">`
 	assert.Equal(t, expected, BehindTheWall.Apply("Behind!")+Chainsaw1)
 }
