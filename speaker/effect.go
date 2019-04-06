7f81d782c7f54583ed68e1faf2ca61a2cd6f3a15
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-04-06 10:41:04 +0300

    Add speaker package.
    Refs #6.

---
+++ speaker/effect.go
@@ -0,0 +1,25 @@
+package speaker
+
+import (
+	"fmt"
+)
+
+// Effect defines an effect you can apply to text.
+// See https://yandex.ru/dev/dialogs/alice/doc/speech-effects-docpage/.
+type Effect string
+
+// Those contants define various effects.
+const (
+	BehindTheWall = Effect("behind_the_wall")
+	Hamster       = Effect("hamster")
+	Megaphone     = Effect("megaphone")
+	PitchDown     = Effect("pitch_down")
+	Psychodelic   = Effect("psychodelic")
+	Pulse         = Effect("pulse")
+	TrainAnnounce = Effect("train_announce")
+)
+
+// Apply wraps given text with effect.
+func (e Effect) Apply(text string) string {
+	return fmt.Sprintf(`<speaker effect="%s">%s<speaker effect="-">`, e, text)
+}
---
+++ speaker/sound.go
@@ -0,0 +1,9 @@
+package speaker
+
+// Those contants define various sounds.
+// See https://yandex.ru/dev/dialogs/alice/doc/sounds/things-docpage/ and subpages.
+const (
+	Chainsaw1 = `<speaker audio="alice-sounds-game-win-1.opus">`
+
+	// TODO add more from https://yandex.ru/dev/dialogs/alice/doc/sounds-docpage/ subpages
+)
