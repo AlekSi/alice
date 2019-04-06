3c8c07622c90a4cb09a3d73fc2e1b98b3cf336f0
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-04-06 10:58:09 +0300

    Add speaker package. (#8)

---
+++ speaker/speaker_test.go
@@ -0,0 +1,13 @@
+package speaker
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEffect(t *testing.T) {
+	expected := `<speaker effect="behind_the_wall">Behind!<speaker effect="-">` +
+		`<speaker audio="alice-sounds-game-win-1.opus">`
+	assert.Equal(t, expected, BehindTheWall.Apply("Behind!")+Chainsaw1)
+}
