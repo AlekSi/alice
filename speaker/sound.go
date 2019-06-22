2a30d4e605e9dcad2be60c6a74ef24586e45b912
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-06-22 14:23:14 +0300

    Add link to the issue.

--- speaker/sound.go
+++ speaker/sound.go
@@ -6,4 +6,5 @@ const (
 	Chainsaw1 = `<speaker audio="alice-sounds-game-win-1.opus">`
 
 	// TODO add more from https://yandex.ru/dev/dialogs/alice/doc/sounds-docpage/ subpages
+	// See https://github.com/AlekSi/alice/issues/6
 )
