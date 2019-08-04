b467ddbf420aad57722338b86376ded77133a18f
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-08-04 12:52:44 +0300

    Add more sound constants.

--- speaker/sound.go
+++ speaker/sound.go
@@ -2,9 +2,42 @@ package speaker
 
 // Those contants define various sounds.
 // See https://yandex.ru/dev/dialogs/alice/doc/sounds/things-docpage/ and subpages.
+//
+// TODO Please contribute more constants. See https://github.com/AlekSi/alice/issues/6
 const (
-	Chainsaw1 = `<speaker audio="alice-sounds-game-win-1.opus">`
-
-	// TODO add more from https://yandex.ru/dev/dialogs/alice/doc/sounds-docpage/ subpages
-	// See https://github.com/AlekSi/alice/issues/6
+	Bell1         = `<speaker audio="alice-sounds-things-bell-1.opus">`
+	Bell2         = `<speaker audio="alice-sounds-things-bell-2.opus">`
+	Car1          = `<speaker audio="alice-sounds-things-car-1.opus">`
+	Car2          = `<speaker audio="alice-sounds-things-car-2.opus">`
+	Chainsaw1     = `<speaker audio="alice-sounds-things-chainsaw-1.opus">`
+	Construction1 = `<speaker audio="alice-sounds-things-construction-1.opus">`
+	Construction2 = `<speaker audio="alice-sounds-things-construction-2.opus">`
+	CuckooClock1  = `<speaker audio="alice-sounds-things-cuckoo-clock-1.opus">`
+	CuckooClock2  = `<speaker audio="alice-sounds-things-cuckoo-clock-2.opus">`
+	Door1         = `<speaker audio="alice-sounds-things-door-1.opus">`
+	Door2         = `<speaker audio="alice-sounds-things-door-2.opus">`
+	Explosion1    = `<speaker audio="alice-sounds-things-explosion-1.opus">`
+	Glass1        = `<speaker audio="alice-sounds-things-glass-1.opus">`
+	Glass2        = `<speaker audio="alice-sounds-things-glass-2.opus">`
+	Gun1          = `<speaker audio="alice-sounds-things-gun-1.opus">`
+	OldPhone1     = `<speaker audio="alice-sounds-things-old-phone-1.opus">`
+	OldPhone2     = `<speaker audio="alice-sounds-things-old-phone-2.opus">`
+	Phone1        = `<speaker audio="alice-sounds-things-phone-1.opus">`
+	Phone2        = `<speaker audio="alice-sounds-things-phone-2.opus">`
+	Phone3        = `<speaker audio="alice-sounds-things-phone-3.opus">`
+	Phone4        = `<speaker audio="alice-sounds-things-phone-4.opus">`
+	Phone5        = `<speaker audio="alice-sounds-things-phone-5.opus">`
+	ShipHorn1     = `<speaker audio="alice-sounds-transport-ship-horn-1.opus">`
+	ShipHorn2     = `<speaker audio="alice-sounds-transport-ship-horn-2.opus">`
+	Siren1        = `<speaker audio="alice-sounds-things-siren-1.opus">`
+	Siren2        = `<speaker audio="alice-sounds-things-siren-2.opus">`
+	Switch1       = `<speaker audio="alice-sounds-things-switch-1.opus">`
+	Switch2       = `<speaker audio="alice-sounds-things-switch-2.opus">`
+	Sword1        = `<speaker audio="alice-sounds-things-sword-1.opus">`
+	Sword2        = `<speaker audio="alice-sounds-things-sword-2.opus">`
+	Sword3        = `<speaker audio="alice-sounds-things-sword-3.opus">`
+	Toilet1       = `<speaker audio="alice-sounds-things-toilet-1.opus">`
+	Water1        = `<speaker audio="alice-sounds-things-water-1.opus">`
+	Water2        = `<speaker audio="alice-sounds-things-water-2.opus">`
+	Water3        = `<speaker audio="alice-sounds-things-water-3.opus">`
 )
